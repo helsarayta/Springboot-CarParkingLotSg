2d09872d56c0549e4ff3e5c1fd3079df8e62e9d8
HeydieZ <helsarayta@gmail.com>
2023-09-29 10:45:39 +0700

    add swagger Documentation

---
+++ src/main/java/com/heydie/parkinglot/config/SwaggerConfig.java
@@ -0,0 +1,41 @@
+package com.heydie.parkinglot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.heydie.parkinglot.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "Get Nearest Available ParkingLot",
+                "Get Nearest Available ParkingLot",
+                "V1",
+                "Terms of Service",
+                new Contact("Heydie Zuliansyah", "helsarayta@gmail.com", "helsarayta@gmail.com"),
+                "",
+                "",
+                Collections.emptyList()
+        );
+        return apiInfo;
+    }
+}
